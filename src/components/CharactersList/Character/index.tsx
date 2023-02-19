bf8ce83b76a0e435c9a9be8f687cebb40aa2280b
sunmin <polarlsm1231@gmail.com>
2023-02-19 21:01:13 +0900

    feat: 삭제 기능 추가

--- src/components/CharactersList/Character/index.tsx
+++ src/components/CharactersList/Character/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import { MiddleButton } from '../../../styles/common';
 import useModal from '../../../hooks/useModal';
@@ -6,18 +6,24 @@ import type { CharacterType } from '../../../types/CharacterType';
 
 export default function Character({ detail }: { detail: CharacterType }) {
   const { setContent, closeModal } = useModal();
+  const [isDelete, setIsDelete] = useState(false);
   const aliases = detail.aliases.join(', ');
   const nameLength = detail.name.length;
   const aliasesEllipsis =
     detail.name.length + aliases.length > 40 ? `${aliases.slice(0, 45 - nameLength)}...` : aliases;
+
   const deleteHandler = () => {
+    setIsDelete(true);
+    closeModal();
+  };
+  const clickDeleteHandler = () => {
     setContent(`정말 삭제하시겠습니까?`, [
-      { name: '확인', handler: closeModal },
+      { name: '확인', handler: deleteHandler },
       { name: '취소', handler: closeModal },
     ]);
   };
   return (
-    <Wrapper>
+    <Wrapper isDelete={isDelete}>
       <Info>
         <InfoLine>
           <span>name: {detail.name.length === 0 ? 'No Name' : detail.name}</span>
@@ -31,19 +37,19 @@ export default function Character({ detail }: { detail: CharacterType }) {
         <InfoLine>gender: {detail.gender}</InfoLine>
       </Info>
       <Delete>
-        <MiddleButton onClick={deleteHandler}>삭제</MiddleButton>
+        <MiddleButton onClick={clickDeleteHandler}>삭제</MiddleButton>
       </Delete>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.article`
+const Wrapper = styled.article<{ isDelete: boolean }>`
   width: 100%;
   height: 84px;
-  display: flex;
   justify-content: space-between;
   border: 1px solid ${({ theme }) => theme.colors.GRAY2};
   margin-bottom: 15px;
+  display: ${({ isDelete }) => (isDelete ? 'none' : 'flex')};
 `;
 
 const Info = styled.div`
